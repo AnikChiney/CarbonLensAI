44b47779ea260e91c0951188be15d960e0fee60a
Shuvam <kundushibam7@gmail.com>
2026-02-18 12:00:12 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(morgan("common"));
 // -------------------- CORS CONFIG -------------------- //
 const allowedOrigins = [
   "http://localhost:3000",                 // local frontend dev
-  "http://localhost:5173",                 // if using Vite
+  //"http://localhost:5173",                 // if using Vite
   "https://carbonlensai-1.onrender.com"    // deployed frontend URL
 ];
 
